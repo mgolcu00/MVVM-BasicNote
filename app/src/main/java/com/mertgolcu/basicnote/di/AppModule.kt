19ef659d19f4af1ee92eeab31ff025180c2d2300
mgolcu <mgolcu00@gmail.com>
2021-04-01 13:33:30 +0300

    Dagger Hilt Modules decomposed.

--- app/src/main/java/com/mertgolcu/basicnote/di/AppModule.kt
+++ app/src/main/java/com/mertgolcu/basicnote/di/AppModule.kt
@@ -1,16 +1,11 @@
 package com.mertgolcu.basicnote.di
 
-import com.mertgolcu.basicnote.api.BasicNoteApi
-import com.mertgolcu.basicnote.utils.BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
-import okhttp3.OkHttpClient
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Qualifier
 import javax.inject.Singleton
 
@@ -18,34 +13,18 @@ import javax.inject.Singleton
 @InstallIn(ApplicationComponent::class)
 object AppModule {
 
-    @Provides
-    @Singleton
-    fun provideRetrofit(
-        @AuthInterceptorOkHttpClient okHttpClient: OkHttpClient
-    ): Retrofit =
-        Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .client(okHttpClient)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-
-    @Provides
-    @Singleton
-    fun provideBasicNoteApi(retrofit: Retrofit): BasicNoteApi =
-        retrofit.create(BasicNoteApi::class.java)
-
     @ApplicationScope
     @Provides
     @Singleton
     fun provideApplicationScope() =
         CoroutineScope(SupervisorJob()) // Learn This specially "Coroutine"
-
-
 }
-
 
 
 @Retention(AnnotationRetention.RUNTIME)
 @Qualifier
 annotation class ApplicationScope
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class AuthInterceptorOkHttpClient
--- app/src/main/java/com/mertgolcu/basicnote/di/NetworkModule.kt
+++ app/src/main/java/com/mertgolcu/basicnote/di/NetworkModule.kt
@@ -1,12 +1,16 @@
 package com.mertgolcu.basicnote.di
 
 import com.mertgolcu.basicnote.api.AuthInterceptor
+import com.mertgolcu.basicnote.api.BasicNoteApi
+import com.mertgolcu.basicnote.utils.BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
-import javax.inject.Qualifier
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -22,9 +26,22 @@ object NetworkModule {
             .build()
     }
 
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        @AuthInterceptorOkHttpClient okHttpClient: OkHttpClient
+    ): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+
+    @Provides
+    @Singleton
+    fun provideBasicNoteApi(retrofit: Retrofit): BasicNoteApi =
+        retrofit.create(BasicNoteApi::class.java)
 
 }
 
-@Qualifier
-@Retention(AnnotationRetention.BINARY)
-annotation class AuthInterceptorOkHttpClient
